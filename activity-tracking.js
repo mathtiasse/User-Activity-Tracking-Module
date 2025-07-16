f3dad99d8ebcd4cd19e33ff98072d2d5eb5789ba
mathtiasse <berger.mathias@hotmail.com>
2025-07-16 13:15:16 +0200

    improve code

--- activity-tracking.js
+++ activity-tracking.js
@@ -1,5 +1,5 @@
 // === CONFIGURATION ===
-var defaultConfig = {
+const defaultConfig = {
   userIdExpirationTime: 13 * 30 * 24 * 60 * 60 * 1000, // 13 months
   visitIdExpirationTime: 30 * 60 * 1000, // 30 minutes
   exclusionList: ["paypal.com", "3dsecure", "wlp-acs", "-3ds-", ".3ds.", "visa.com"],
@@ -8,103 +8,93 @@ var defaultConfig = {
   sourceParams: ["utm_source", "utm_medium", "utm_campaign", "utm_id", "utm_term", "utm_content"]
 };
 
-window.initActivityTracking = function(userConfig) {
-  var config = Object.assign({}, defaultConfig, userConfig);
+window.initActivityTracking = (userConfig = {}) => {
+  const config = { ...defaultConfig, ...userConfig };
 
   // === UTILS ===
-  function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+  const generateUUID = () =>
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
-  }
 
-  function isExpired(item, duration) {
-    return item === null || item.expirationDate < Date.now();
-  }
+  const isExpired = (item, duration) =>
+    !item || item.expirationDate < Date.now();
 
-  function setCookie(name, value, expiresTimestamp) {
-    var expires = "";
-    if (expiresTimestamp) {
-      var date = new Date(expiresTimestamp);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
-  }
+  const setCookie = (name, value, expiresTimestamp) => {
+    const expires = expiresTimestamp
+      ? `; expires=${new Date(expiresTimestamp).toUTCString()}`
+      : "";
+    document.cookie = `${name}=${encodeURIComponent(value)}${expires}; path=/`;
+  };
 
-  function getFromStorage(key) {
-    try { return JSON.parse(localStorage.getItem(key)); } catch (e) { return null; }
-  }
+  const getFromStorage = key => {
+    try { return JSON.parse(localStorage.getItem(key)); } catch { return null; }
+  };
 
-  function setInStorage(key, value) {
-    try { localStorage.setItem(key, JSON.stringify(value)); } catch (e) {}
-  }
+  const setInStorage = (key, value) => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  };
 
-  function isInternalReferrer(referrer) {
-    try { return new URL(referrer).hostname === window.location.hostname; } catch (e) { return true; }
-  }
-
-  function isExcludedReferrer(referrer) {
+  const isInternalReferrer = referrer => {
     try {
-      var host = new URL(referrer).hostname;
-      return config.exclusionList.some(function (ex) { return host.indexOf(ex) !== -1; });
-    } catch (e) {
+      return new URL(referrer).hostname === window.location.hostname;
+    } catch {
+      return true;
+    }
+  };
+
+  const isExcludedReferrer = referrer => {
+    try {
+      const host = new URL(referrer).hostname;
+      return config.exclusionList.some(ex => host.includes(ex));
+    } catch {
       return false;
     }
-  }
+  };
 
-  function sourcesAreDifferent(a, b) {
-    if (!a || !b) return true;
-    for (var key in a) {
-      if (a[key] !== b[key]) return true;
-    }
-    for (var key in b) {
-      if (a[key] !== b[key]) return true;
-    }
-    return false;
-  }
+  const sourcesAreDifferent = (a = {}, b = {}) => {
+    const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+    return Array.from(keys).some(key => a[key] !== b[key]);
+  };
 
-  function isValidNewSource(source) {
+  const isValidNewSource = source => {
     if (!source) return false;
-    var hasRecognizedParam = config.sourceParams.some(function (key) {
-      return source[key];
-    });
+    const hasRecognizedParam = config.sourceParams.some(key => !!source[key]);
     return hasRecognizedParam || !!source.referrer;
-  }
+  };
 
-  function buildSource() {
-    var source = {};
-    var ref = document.referrer;
+  const buildSource = () => {
+    const source = {};
+    const ref = document.referrer;
     if (!isInternalReferrer(ref) && !isExcludedReferrer(ref)) {
       source.referrer = ref;
     }
-    var params = new URLSearchParams(window.location.search);
-    config.sourceParams.forEach(function (key) {
-      var val = params.get(key);
+    const params = new URLSearchParams(window.location.search);
+    config.sourceParams.forEach(key => {
+      const val = params.get(key);
       if (val) source[key] = val;
     });
     return source;
-  }
+  };
 
-  function detectDeviceType() {
-    var ua = navigator.userAgent;
-    var isTablet = /Tablet|iPad|Nexus 7|Nexus 10|SM-T|GT-P|Kindle|Silk/i.test(ua);
-    var isMobile = /Mobi|Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua) && !isTablet;
-    if (isTablet) return "tablet";
-    if (isMobile) return "mobile";
-    return "desktop";
-  }
+  const detectDeviceType = () => {
+    const ua = navigator.userAgent;
+    const isTablet = /Tablet|iPad|Nexus 7|Nexus 10|SM-T|GT-P|Kindle|Silk/i.test(ua);
+    const isMobile = /Mobi|Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua) && !isTablet;
+    return isTablet ? "tablet" : isMobile ? "mobile" : "desktop";
+  };
 
   // === MAIN ===
   window.dataLayer = window.dataLayer || [];
-  var now = Date.now();
-  var landingPage = window.location.href;
-  var currentSource = buildSource();
-  var deviceType = detectDeviceType();
-  var currentUserId = getFromStorage("userActivity");
+  const now = Date.now();
+  const landingPage = window.location.href;
+  const currentSource = buildSource();
+  const deviceType = detectDeviceType();
+  let currentUserId = getFromStorage("userActivity");
 
-  var isNewUser = false, isNewVisit = false, isNewSource = false;
-  var previousSource = currentUserId && currentUserId.visit && currentUserId.visit.source
+  let isNewUser = false, isNewVisit = false, isNewSource = false;
+  const previousSource = currentUserId?.visit?.source
     ? JSON.parse(JSON.stringify(currentUserId.visit.source))
     : null;
 
@@ -114,12 +104,12 @@ window.initActivityTracking = function(userConfig) {
       id: generateUUID(),
       expirationDate: now + config.userIdExpirationTime,
       lastActivity: now,
-      landingPage: landingPage,
+      landingPage,
       source: currentSource,
       visit: {
         id: generateUUID(),
         expirationDate: now + config.visitIdExpirationTime,
-        landingPage: landingPage,
+        landingPage,
         source: currentSource,
         count: 1,
         isEngaged: false
@@ -131,9 +121,9 @@ window.initActivityTracking = function(userConfig) {
       currentUserId.visit = {
         id: generateUUID(),
         expirationDate: now + config.visitIdExpirationTime,
-        landingPage: landingPage,
+        landingPage,
         source: currentSource,
-        count: (currentUserId.visit && currentUserId.visit.count || 0) + 1,
+        count: (currentUserId.visit?.count || 0) + 1,
         isEngaged: false
       };
     } else {
@@ -156,19 +146,16 @@ window.initActivityTracking = function(userConfig) {
   window.dataLayer.push({
     event: "page_load",
     userActivity: JSON.parse(JSON.stringify(currentUserId)),
-    deviceType: deviceType
+    deviceType
   });
 
-  if (isNewUser) {
-    window.dataLayer.push({ event: "first_visit" });
-  }
+  if (isNewUser) window.dataLayer.push({ event: "first_visit" });
   if (isNewVisit) {
     window.dataLayer.push({ event: "visit_start" });
 
-    // visit_engaged timer
-    setTimeout(function () {
-      var updated = getFromStorage("userActivity");
-      if (updated && updated.visit && updated.visit.id === currentUserId.visit.id) {
+    setTimeout(() => {
+      const updated = getFromStorage("userActivity");
+      if (updated?.visit?.id === currentUserId.visit.id) {
         updated.visit.isEngaged = true;
         setInStorage("userActivity", updated);
         window.dataLayer.push({
@@ -178,6 +165,7 @@ window.initActivityTracking = function(userConfig) {
       }
     }, config.visitEngagementDelay);
   }
+
   if (isNewSource && !isNewVisit) {
     window.dataLayer.push({ event: "new_visit_source" });
   }
